7deedf0cbf50c697312bb8e190807e4c549c4a90
lysanderkirkland <lysander.kirkland@gmail.com>
2021-05-23 13:58:31 +1000

    add cms

--- src/main.js
+++ src/main.js
@@ -6,4 +6,16 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  // Add an external JavaScript before the closing </body> tag
+  head.script.push({
+    src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
+    body: true
+  })
+
+  head.script.push({
+    src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
+    body: true
+  })
+
 }
