88b389d582307951b0c82fe73514f90ae380c687
lysanderkirkland <lysander.kirkland@gmail.com>
2021-06-05 14:15:10 +1000

    test

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,7 @@
     <h1>Hello, world!</h1>
 
     <p>
-      aLorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
 
     <ul>
